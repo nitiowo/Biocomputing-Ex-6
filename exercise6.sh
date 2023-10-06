d2d407cb0c25f8a77ed0a7fa4e97479f8054fcde
kjackows <kjackows@nd.edu>
2023-10-06 01:26:16 -0400

    Finished Exercise 6

--- exercise6.sh
+++ exercise6.sh
@@ -14,4 +14,26 @@ cat genderyearmatches.csv | sed 's/,/ /g' > genderyearmatches.txt
 sort -k1,1 -k2,2n genderyearmatches.txt > finalgenderyearmatches.txt
 
 
-### Task 2: return following info to stdout 
+### Task 2: return following info to stdout with labels of what they are
+#a) gender, yearsExperience, and wage for highest earner
+
+cat wages.csv |cut -d , -f 1,2,4 | sed 's/,/ /g'| sort -k 3n | tail -n 1 
+
+#b) gender, yearsExperience, and wage for lowest earner
+
+cat wages.csv |cut -d , -f 1,2,4 | sed 's/,/ /g'| sort -k 3n | head -n 2
+
+#c) number of females in top 10 earners in data set
+
+cat wages.csv |cut -d , -f 1,2,4 | sed 's/,/ /g'| sort -k 3n | tail -n 10 | grep -E "female" | wc -l
+
+### Task 3: return to stdout the effect of graduating college (12 vs 16 years of school) on min wage for earners in the dataset
+##... This means to find the lowest min wage earner for 12 years of school and 16 and subtract those earnings 
+
+#first need to find each of these 2 values, assign them to variables to store them and then do subtraction with the 2 variables to find the answer
+
+low12wage=$(cat wages.csv | cut -d , -f 3,4 | grep -E "^12" | sed 's/,/ /g' | sort -k 2n | head -n 1 | cut -d " " -f 2)
+
+low16wage=$(cat wages.csv | cut -d , -f 3,4 | grep -E "^16" | sed 's/,/ /g' | sort -k 2n | head -n 1 | cut -d " " -f 2)
+
+echo "$low16wage - $low12wage" | bc
